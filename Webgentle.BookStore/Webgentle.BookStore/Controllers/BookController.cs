0d612dd7cd60d04a06fdcdda6b7db7538d9c374c
mohammad <ms.ghazi253@gmail.com>
2021-04-08 08:48:01 -0700

    Controller in aspnetCore

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "Get All Books books";
+        }
+        public string GetBook(int id)
+        {
+            return $"book with id ={id}";
+        }
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Boom with name = {bookName} & Author = {authorName}";
+        }
+    }
+}
