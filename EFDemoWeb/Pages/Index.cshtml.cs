53d45f5142479d370e2ea52c60bb250b4543fc60
mohammad <ms.ghazi253@gmail.com>
2021-05-18 05:17:39 -0700

    Add project files.

---
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EFDemoWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
+
+        public IndexModel(ILogger<IndexModel> logger , PeopleContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        private void LoadSampleDate()
+        {
+            if (_db.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _db.AddRange(people);
+                _db.SaveChanges(); 
+            }
+        }
+    }
+}
