ae9040dc01e85bfe8e0809170d72784ba14c022e
mohammad <ms.ghazi253@gmail.com>
2021-05-26 06:00:00 -0700

    0003

--- Webgentle.BookStore/Webgentle.BookStore/Data/BookStoreContext.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Data/BookStoreContext.cs
@@ -19,14 +19,4 @@ namespace Webgentle.BookStore.Data
             base.OnConfiguring(optionsBuilder);
         }
     }
-    public class Books
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public string Language { get; set; }
-        public int TotalPages { get; set; }
-    }
 }
